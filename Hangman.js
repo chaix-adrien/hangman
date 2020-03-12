9a24cb33ee5a1c33f1862649da828c4d3196c813
Adrien Chaix <chaix.adrien@laposte.net>
2020-03-12 15:55:17 +0100

    hover

--- Hangman.js
+++ Hangman.js
@@ -5,36 +5,57 @@ import hangImgs from "./assets/hangman"
 class Hangman extends React.Component {
   constructor(props) {
     super(props);
-    this.word = props.word.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().split("")
-    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this.word = props.word.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase().split("")
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
     this.state = {
       errors: 0,
       used: [],
+
       string: this.word.map(l => l.toLowerCase() === l.toUpperCase() ? l : '*')
     }
     console.log(this.word)
   }
 
+  onLose = () => {
+    console.log("LOOSE")
+  }
+
+  onWin = () => {
+    console.log("WIN")
+  }
+
   useLetter = (l) => {
-    this.setState({ used: [...this.state.used, l] })
+    let error = 0
+    if (!this.word.some((s) => s === l))
+      error++
+    if (this.state.errors + error === hangImgs.length - 1)
+      setTimeout(this.onLose, 2000)
+    const newString = this.word.map((s, id) => s === l ? s : this.state.string[id])
+    console.log(newString.join(""), this.word)
+    if (newString.join("") === this.word.join(""))
+      setTimeout(this.onWin, 2000)
+    this.setState({ used: [...this.state.used, l], errors: this.state.errors + error, string: newString })
   }
 
   render() {
     const { errors, string, used } = this.state
     return (
       <View style={styles.container}>
-        <Image source={hangImgs[errors]} style={styles.img} resizeMode="contain" />
+        <View style={styles.imgCont}>
+          <Image source={hangImgs[errors]} style={styles.img} resizeMode="contain" />
+          <Image source={hangImgs[hangImgs.length - 1]} style={[styles.img, styles.imgHover]} resizeMode="contain" />
+        </View>
         <View style={styles.stringContainer}>
           <Text style={styles.string}>{string.join("")}</Text>
         </View>
-        <View style={styles.lettersContainer}>
+        {errors < hangImgs.length - 1 && <View style={styles.lettersContainer}>
           {this.alphabet.map(l => {
             const use = used.includes(l)
             return <TouchableOpacity style={[styles.letterContainer, use ? styles.letterUse : {}]} onPress={() => this.useLetter(l)} disabled={use} >
               <Text style={[styles.letter, { color: use ? "grey" : "black", }]}>{l}</Text>
             </TouchableOpacity>
           })}
-        </View>
+        </View>}
       </View >
     );
   }
@@ -50,11 +71,16 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     padding: 20
   },
-  img: {
+  imgCont: {
     height: "40%",
     width: "70%",
-    backgroundColor: "red",
     maxWidth: 700,
+  },
+  img: {
+width: "100%", height: "100%"
+  },
+  imgHover: {
+opacity: 0.1, top: 0, left: 0, right: 0, bottom: 0, position: "absolute"
   },
   stringContainer: {
     flexDirection: "row",
